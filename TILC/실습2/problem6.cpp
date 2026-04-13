75325273d85158258082d2b3201fb7420184be39
minjjun0317 <minjjun0317@gmail.com>
2026-04-13 17:58:30 +0900

    0413

---
+++ TILC/실습2/problem6.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+unsigned long long ganadi(int n)
+{
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int main()
+{
+    int n;
+    printf("알고 싶은 팩토리얼의 값은? ");
+    scanf("%d", &n);
+
+    unsigned long long value = ganadi(n);
+    printf("%d!의 값은 %llu 입니다.\n", n, value);
+    return 0;
+}
+ㄴ
